9f1508c936217c6dd8e42b6a59779e1f559d6944
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2015-01-15 17:19:01 +0700

    update with Twitter auth

--- api/services/twitter.js
+++ api/services/twitter.js
@@ -1,14 +1,20 @@
 var Twitter = require('twitter'),
   Promise = require('bluebird');
 
-var client = new Twitter({
-  consumer_key: 'Ru17p9AvxtGWqzsd938ceCcVt',
-  consumer_secret: 'QGwjxIQ08b4LFkmnKh9HXgCxMSqDErlUGCKM4hsiXBBVzFoKTt',
-  access_token_key: '85220340-LgeW2o1XvW8pD25IOSYlMkazJZFVkGXLpJp0f6Vch',
-  access_token_secret: 'xysVna9kYHpXSqIb2w1uMQTtYvKcsX6QsA7qdxVWrWhCw'
-});
-
-module.exports.getUser = function(ids){
+module.exports.getUser = function(req, ids){
+  var token = null,
+    tokenSecret = null;
+  Passport.findOne({user:req.user.id,provider:'twitter'})
+    .exec(function(err, rec){
+      token = rec.tokens.token;
+      tokenSecret = rec.tokens.tokenSecret;
+    });
+  var client = new Twitter({
+    consumer_key: 'Ru17p9AvxtGWqzsd938ceCcVt',
+    consumer_secret: 'QGwjxIQ08b4LFkmnKh9HXgCxMSqDErlUGCKM4hsiXBBVzFoKTt',
+    access_token_key: token,
+    access_token_secret: tokenSecret
+  });
   return new Promise(function(resolve, reject){
     client.post('users/lookup.json', {
       screen_name: ids
