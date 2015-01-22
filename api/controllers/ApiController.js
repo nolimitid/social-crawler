a04d972233b01a9f924c2f35c9875e760a96d136
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2015-01-22 14:50:18 +0700

    update generate sql

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -136,30 +136,40 @@ module.exports = {
       reduced[group].push(n);
     });
 
-    sails.log.verbose('Getting Facebook\'s users divided into '+reduced.length+' groups');
+    sails.log.verbose('Getting Facebook\'s users divided into ' + reduced.length + ' groups');
     _.each(reduced, function (n, i) {
       var ids = n.join(',');
-      promises.push(facebook.getUser(ids,i));
+      promises.push(facebook.getUser(ids, i));
     });
 
     Promise.all(promises)
-      .then(function(results, rejects){
+      .then(function (results, rejects) {
         //console.log(results);
-        var flatten = _.flatten(_.map(results, function(n){
-          return _.map(n, function(o){
+        var flatten = _.flatten(_.map(results, function (n) {
+          return _.map(n, function (o) {
             return o;
           });
         }));
         //console.log(flatten);
-        var require = _.map(flatten, function(n){
+        var require = _.map(flatten, function (n) {
           return {
             object_name: n.name || '',
             content: n.id || '',
             extra_content: n.id || ''
           }
         });
-        if(rejects) return res.json(rejects);
+        if (rejects) return res.json(rejects);
         res.json(require);
       });
+  },
+  sql: function (req, res) {
+    var media = req.param('media') || null;
+    if (!media) return res.ok('parameter \'media\' harap diisi \'facebook\' atau \'twitter\'');
+
+    var file = JSON.parse(fs.readFileSync('./file/' + media + '.json'));
+    var sql = _.map(file, function (n) {
+      return '(object_name,media_id,stream_type,content,extra_content,app_id,date_created) VALUES ("' + n.object_name + '","' + media + '","account","' + n.content + '","' + n.extra_content + '", @APPID, now())'
+    }).join(',');
+    res.ok(sql);
   }
 };
